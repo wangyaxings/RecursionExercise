63aefb63d242cb28b7c22ab64109709ca5220c2a
wyx <wyx@DESKTOP-87U6JDN>
2022-03-25 23:38:42 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//递归之阶乘
+
+int RecursionFactorial(int start, int end)
+{
+    return start;
+     
+}
+
+Console.WriteLine(RecursionFactorial(1,10));
+Console.ReadKey();
