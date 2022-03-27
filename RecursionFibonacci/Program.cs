94169b37737461db8eb9eb28a2343faf72318e44
wyx <wangyaxing007@outlook.com>
2022-03-27 23:36:03 +0800

    wyx-20220327-Feature: Add new feature recursion Fibonacci and rename the project

---
+++ RecursionFibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 斐波那契数列 ：1、1、2、3、5、8、13、21、34
+ */
+namespace RecursionFibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(9));
+            Console.ReadKey();
+        }
+        static int Fibonacci(int n)
+        {
+            //判断n的合法性（略）
+            if (n < 3)
+            {
+                return 1;
+            }
+            else 
+            {
+                int x = Fibonacci(n - 1);
+                int y = Fibonacci(n - 2);
+                return x + y;
+                //return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+            
+        }
+    }
+}
