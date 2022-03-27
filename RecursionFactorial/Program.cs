6e1783d3731def6e6694b1363f30a29708bc641e
wyx <wangyaxing007@outlook.com>
2022-03-27 23:38:59 +0800

    wyx-20220325-Feature:Add the 1st day new feature Fcatorial

---
+++ RecursionFactorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 01-计算 x到y的乘积
+ */
+namespace RecursionFactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RecursionFactorial(1,10));
+            Console.ReadKey();
+        }
+        static int RecursionFactorial(int start,int end)
+        {
+            if (start == end)
+            {
+                return start;
+            }
+            else
+            {
+                return start*RecursionFactorial(start+1,end);
+            }
+        }
+    }
+}
