598a5d16eef7e6a2b523099887ef98da7c50d687
wyx <wangyaxing007@outlook.com>
2022-03-30 22:15:19 +0800

    wyx-20220330-Feature: Add a new feature Strip the backspace in the head and tail

---
+++ 06-RecursionStrip/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 删除字符串两端的空格，功能类似python语法中的strip
+ */
+namespace _06_RecursionStrip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testStr = "    assert n > 0 and type(n) is int  ";
+            string retStr = Strip(testStr);
+            Console.WriteLine(retStr);
+            Console.ReadKey();
+        }
+
+        private static string Strip(string testStr)
+        {
+            if (!(testStr.StartsWith(" ") || testStr.EndsWith(" ")))
+            {
+                return testStr;
+            }
+            else
+            {
+                if (testStr.StartsWith(" "))
+                {
+                    testStr = testStr.Substring(1, testStr.Length - 1);                    
+                }
+                if (testStr.EndsWith(" "))
+                {
+                    testStr = testStr.Substring(0, testStr.Length - 1);                    
+                }
+                return Strip(testStr);
+            }            
+        }
+    }
+}
