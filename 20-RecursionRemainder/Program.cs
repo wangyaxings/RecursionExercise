30f514cb46d5627b522b61ff7c0589daff173f39
wyx <wangyaxing007@outlook.com>
2022-04-13 23:45:55 +0800

    wyx-20220413-Feature: add a new feature remainder calculate

---
+++ 20-RecursionRemainder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 计算余数，但不使用%
+ */
+namespace _20_RecursionRemainder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 27;
+            int y = 3;
+            int remainder = Remainder(x,y);
+            Console.WriteLine($"{x}%{y} = {remainder} ");
+            Console.ReadKey();
+        }
+
+        private static int Remainder(int v1, int v2)
+        {
+            if (v1 < v2)
+            {
+                return v1;
+            }
+            else
+            {
+                //除法可以理解为减法
+                return Remainder(v1-v2, v2);
+            }
+        }
+    }
+}
