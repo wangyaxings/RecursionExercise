4bef5d5cd7bc7c98b3adcabb8011434d13f89d00
wyx <wangyaxing007@outlook.com>
2022-04-23 00:26:07 +0800

    wyx-20220422-Feature: add a new feature of WPF logical tree traversal

---
+++ 29-RecursionWPFLogicalTreeTraversal/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace _29_RecursionWPFLogicalTreeTraversal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.logicTree.Items.Add(GetLogicalTree(this));
+        }
+        public TreeViewItem GetLogicalTree(DependencyObject dependencyObject)
+        {
+            if (dependencyObject == null)
+            {
+                return null;
+            }
+            else
+            {
+                TreeViewItem treeViewItem = new TreeViewItem() { Header= dependencyObject.GetType().FullName, IsExpanded=true};
+                foreach (var child in LogicalTreeHelper.GetChildren(dependencyObject))
+                {
+                    var node = GetLogicalTree(child as DependencyObject);
+                    if (node != null)
+                    {
+                        treeViewItem.Items.Add(node);
+                    }                    
+                }
+                return treeViewItem;
+            }            
+        }
+    }
+}
