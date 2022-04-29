56577823f785d031217b67449d6ab8287a8fddfc
wyx <wangyaxing007@outlook.com>
2022-04-30 00:34:36 +0800

    wyx-20220429-Feature: add a new feature cell division

---
+++ 36-RecursionCellDivision/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 细胞分裂中，一个细胞每一个小时分裂一次，一次分裂一个子细胞，第三个小时后消亡，n小时后有多少细胞？
+ */
+namespace _36_RecursionCellDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int sum = CellDivision(i);
+                Console.WriteLine($"After {i}h,{sum} cells left");
+            }
+            
+            Console.ReadKey();
+        }
+        //这里计算的是hours之后的细胞个数
+        private static int CellDivision(int hours)
+        {
+            if (hours == 0)
+            {
+                return 1;
+            }
+            else if (hours == 1)
+            {
+                return 2;
+            }
+            else if (hours == 2)
+            {
+                return 4;
+            }
+            else
+            {
+                return CellDivision(hours - 1) + CellDivision(hours - 2) + CellDivision(hours - 3);
+            }
+        }
+    }
+}
