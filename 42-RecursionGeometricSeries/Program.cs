3a0de6916b61477d0620a9fc57fa9467ede88a2f
wyx <wangyaxing007@outlook.com>
2022-05-06 00:54:20 +0800

    wyx-20220505-Feature: add a new feature geometric series

---
+++ 42-RecursionGeometricSeries/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*
+ * 等比数列 f(n) = 2*f(n-1), f(1) = 1
+ */
+namespace _42_RecursionGeometricSeries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                int ret = GeometricSeries(i);
+                Console.WriteLine($"{i} ~ {ret}");
+            }
+
+            Console.ReadKey();            
+        }
+
+        private static int GeometricSeries(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return 2*GeometricSeries(number-1);
+            }
+        }
+    }
+}
