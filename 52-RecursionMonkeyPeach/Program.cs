77d83306262ca9b824dc73c3918567313fdef25d
wyx <wangyaxing007@outlook.com>
2022-05-16 01:22:01 +0800

    wyx-20220515-Feature: add a new feature Monkey peach

---
+++ 52-RecursionMonkeyPeach/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 一只猴子吃n个桃子，每天吃现有桃子的一半多一个，第10天还有一个，求n
+ */
+namespace _52_RecursionMonkeyPeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{MonkeyPeach(1)}");
+            Console.ReadKey();
+        }
+
+        private static int MonkeyPeach(int day)
+        {            
+            if (day==10)
+            {
+                return 1;
+            }
+            else
+            {
+                return (MonkeyPeach(day + 1) + 1)*2;
+            }
+        }
+    }
+}
